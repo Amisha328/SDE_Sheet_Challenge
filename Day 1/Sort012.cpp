6cfbbdf9d21f593cb523ae34d9674a7795ebed22
Amisha Sahu <amishasahu328@gmail.com>
2022-06-05 16:31:54 +0530

    Sort 0 1 2 with Explanation

--- Day 1/Sort012.cpp
+++ Day 1/Sort012.cpp
@@ -2,11 +2,65 @@
 /*
 The solution requires the use of tracking 3 positions, the Low, Mid and High.
 
-We assume that the mid is the "Unknown" area that we must evaluate.
-
 If we encounter a 0, we know that it will be on the low end of the array, and if we encounter a 2, we know it will be on the high end of the array.
 
-To achieve this in one pass without preprocessing (counting), we simply traverse the unknown(mid) will generating the low and high ends.
+To achieve this in one pass without preprocessing (counting), we simply traverse the mid pointer that will be generating the low and high ends.
+*/
+
+/*
+Example:
+Input: 1 0 2 2 1 0 .
+
+Explanation:
+
+    1 0 2 2 1 0
+    ^         ^
+    L         H
+    M
+
+    Mid == 1
+    Mid++
+
+    1 0 2 2 1 0
+    ^ ^       ^
+    L M       H
+
+    Mid == 0
+    Swap Low and Mid
+    Mid++
+    Low++
+
+    0 1 2 2 1 0
+      ^ ^     ^
+      L M     H
+
+    Mid == 2
+    Swap High and Mid
+    High--
+
+    0 1 0 2 1 2
+      ^ ^   ^
+      L M   H
+
+    Mid == 0
+    Swap Low and Mid
+    Mid++
+    Low++
+
+    0 0 1 2 1 2
+        ^ ^ ^
+        L M H
+
+    Mid == 2
+    Swap High and Mid
+    High--
+
+    0 0 1 1 2 2
+        ^ ^
+        L M
+          H
+
+    Mid <= High is our exit case
 */
 
 // TC-> O(nlogn) | SC-> O(1)
