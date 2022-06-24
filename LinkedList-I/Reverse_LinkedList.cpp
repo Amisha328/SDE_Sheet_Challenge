5a4f52607c7d56bc20257979e29f86886a584105
Amisha Sahu <amishasahu328@gmail.com>
2022-06-24 16:06:22 +0530

    Reverse Linked List

---
+++ LinkedList-I/Reverse_LinkedList.cpp
@@ -0,0 +1,30 @@
+// Reverse Linked List
+
+#include <bits/stdc++.h> 
+
+template <typename T>
+class LinkedListNode
+{
+public:
+        T data;
+        LinkedListNode<T> *next;
+        LinkedListNode(T data)
+        {
+            this->data = data;
+            this->next = NULL;
+        }
+};
+
+LinkedListNode<int> *reverseLinkedList(LinkedListNode<int> *head) 
+{
+    LinkedListNode<int> *curr = head, *prev = NULL, *next = NULL;
+    while(curr != NULL)
+    {
+        next = curr->next;
+        curr-> next = prev;
+        prev = curr;
+        curr = next;
+    }
+    return prev;
+}
+
