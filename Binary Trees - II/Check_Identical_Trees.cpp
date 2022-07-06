a390b6413fee95aa4bce531603c0531a8d7443ef
Amisha Sahu <amishasahu328@gmail.com>
2022-07-06 15:26:51 +0530

    Check Identical Trees

---
+++ Binary Trees - II/Check_Identical_Trees.cpp
@@ -0,0 +1,17 @@
+// Check Identical Trees
+
+// Idea:
+// Two trees are identical if:
+// Preorder/Inorder/Postorder of Tree1 = Preorder/Inorder/Postorder of Tree2
+
+#include <bits/stdc++.h>
+bool identicalTrees(BinaryTreeNode<int>* root1, BinaryTreeNode<int>* root2) {
+    // Write your code here. 	
+    if(root1 == NULL || root2 == NULL)
+            return (root1 == root2);
+        
+    if(root1->data != root2->data)
+            return false;
+        
+    return (identicalTrees(root1->left, root2->left) && identicalTrees(root1->right, root2->right));
+}
