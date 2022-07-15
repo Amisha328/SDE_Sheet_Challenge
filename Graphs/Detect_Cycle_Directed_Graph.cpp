33ab077ab497e9d1920ecacbd29de01c8a549cd0
Amisha Sahu <amishasahu328@gmail.com>
2022-07-15 16:54:31 +0530

    Detect Cycle In A Directed Graph

--- Graphs/Detect_Cycle_Directed_Graph.cpp
+++ Graphs/Detect_Cycle_Directed_Graph.cpp
@@ -2,7 +2,7 @@
 
 #include <bits/stdc++.h>
 
-bool detectCycle(int &start, vector<int> adj[], vector<int> &visited, vector<int> &recStack)
+bool detectCycle(int start, vector<int> adj[], vector<int> &visited, vector<int> &recStack)
 {
         visited[start] = 1;
         recStack[start] = 1;
@@ -17,29 +17,21 @@ bool detectCycle(int &start, vector<int> adj[], vector<int> &visited, vector<int
         return false;
 }
 
-bool isCyclic(int V, vector<int> adj[]) {
-        vector<int> visited(V, 0);
-        vector<int> recStack(V, 0);
-        for(int i = 0; i < V; i++)
-        {
-            if(!visited[i] && detectCycle(i, adj, visited, recStack))
-			{
-				//cout<<i<<"True";
-				 return true;
-			}
-        }
-        return false;
-}
 int detectCycleInDirectedGraph(int n, vector <pair< int, int >> & edges) {
-	// Write your code here.
-	vector<int> adj[n];
-
+	vector<int> adj[n+1];
+    vector<int> visited(n+1, 0);
+    vector<int> recStack(n+1, 0);
 	for(auto x: edges){
 		adj[x.first].push_back(x.second);
-		adj[x.second].push_back(x.first);
 	}
 	
-	//cout<< isCyclic(n, adj);
-	return isCyclic(n, adj);
-	
+	for(int i = 1; i <= n; i++)
+    {
+            if(!visited[i] && detectCycle(i, adj, visited, recStack))
+            {
+                //cout<<i<<"True";
+                 return true;
+            }
+     }
+     return false;
 }
